94c260bcff182c3827af6cf3319d6ea5aafe0b12
victormuteithia <muteithiavictor@gmail.com>
2024-03-15 08:41:45 +0300

    Assignment in progress

---
+++ lib/challenge_1.py
@@ -0,0 +1,12 @@
+#Challenge 1
+
+# def solution(array):
+#     for num in array:
+#         if num > 10:
+#             return num - 1
+#         elif num < 10:
+#             return num + 1
+#         elif num == 10:
+#             return num
+
+
---
+++ lib/challenge_2.py
@@ -0,0 +1,13 @@
+#Challenge 2
+#pseudo code
+#START
+#define the sum of the digits
+
+    
+
+def solution(array):
+    num_list = []
+    for num in array:
+        num_list.append(str(num))
+    return num_list
+
